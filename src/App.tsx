c8c2267efb9c98b3e0b23e6e0041597f17e40a76
labyvaby <labyvaby@gmail.com>
2026-09-10 13:47:56 +0600

    fix(modules): hide disabled sections for superadmins

--- src/App.tsx
+++ src/App.tsx
@@ -589,9 +589,11 @@ function App() {
                           path="dashboard"
                           element={
                             <RequireSuperAdmin>
-                              <Suspense fallback={<LinearProgress />}>
-                                <DashboardPage />
-                              </Suspense>
+                              <RequirePermission permission={PAGE_PERMISSIONS.reports}>
+                                <Suspense fallback={<LinearProgress />}>
+                                  <DashboardPage />
+                                </Suspense>
+                              </RequirePermission>
                             </RequireSuperAdmin>
                           }
                         />
