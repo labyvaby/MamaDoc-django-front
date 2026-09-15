b54185f0d24aeb2c0bcf5fd417a8903dd333cac0
labyvaby <labyvaby@gmail.com>
2026-09-15 16:09:26 +0600

    Исправить активный пункт POS в меню

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -820,7 +820,7 @@ const SidebarSecondary: React.FC = () => {
         {/* Касса магазина — рабочий инструмент продаж, в разделе «Моя работа» */}
         {show("my-work") && can_.pos && (
           <>
-            <SidebarMenuItem to="/pos" icon={<PointOfSaleOutlined />} label="Касса магазина" collapsed={siderCollapsed} />
+            <SidebarMenuItem to="/pos" icon={<PointOfSaleOutlined />} label="Касса магазина" collapsed={siderCollapsed} excludePaths={["/pos/history"]} />
             <SidebarMenuItem to="/pos/history" icon={<HistoryOutlined />} label="История продаж" collapsed={siderCollapsed} />
           </>
         )}
